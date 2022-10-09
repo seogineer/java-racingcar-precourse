818ece64dce2b9538a1b7abb00ea63e2cb5fe4eb
seogineer <dgseo8981@gmail.com>
2022-10-09 15:06:05 +0900

    refactor(View): 입력 받은 자동차 이름과 횟수를 유효성 검사하고 반환하는 기능으로 수정 및 수정 사항에 따라 테스트 코드 수정
    변경 내용
    - 입력 받은 이름을 배열로 반환
    - 입력 받은 횟수 유효성 검사(음수 또는 영문인 경우 예외처리)
    - 입력 받은 횟수 int 타입으로 반환

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -1,15 +1,28 @@
 package racingcar.view;
 
+import java.util.regex.Pattern;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class View {
-    public void inputCarNameView(){
+    public String[] inputCarNameView(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return readLine().split(",");
     }
 
-    public void inputTryCountView() {
-        System.out.println("시도할 회수는 몇회인가요?");
+    public int inputTryCountView() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return Integer.parseInt(tryCountValidate(readLine()));
     }
 
-    public void resultView() {
+    public void outputResultView() {
         System.out.println("실행 결과");
+    }
+
+    private String tryCountValidate(String count){
+        if(!Pattern.matches("^[0-9]*$", count)){
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+        }
+        return count;
     }
 }
--- src/test/java/racingcar/view/ViewTest.java
+++ src/test/java/racingcar/view/ViewTest.java
@@ -2,40 +2,79 @@ package racingcar.view;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ViewTest {
+    private View view = new View();
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-
-    private final View view = new View();
 
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
-    @Test
-    void 자동차_이름_입력_화면(){
-        view.inputCarNameView();
-        assertEquals("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)", outputStreamCaptor.toString()
-                .trim());
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,honux,crong"})
+    void 자동차_이름_입력_화면(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        new Scanner(System.in);
+
+        String[] names = view.inputCarNameView();
+        assertEquals("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+                outputStreamCaptor.toString().trim());
+        assertEquals(3, names.length);
+        assertEquals("pobi", names[0]);
+        assertEquals("honux", names[1]);
+        assertEquals("crong", names[2]);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"5"})
+    void 시도_횟수_입력_화면(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        new Scanner(System.in);
+
+        assertEquals(5, view.inputTryCountView());
+        assertEquals("시도할 회수는 몇회인가요?", outputStreamCaptor.toString().trim());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1"})
+    void 시도_횟수_음수_입력(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        new Scanner(System.in);
+
+        assertThatThrownBy(() -> view.inputTryCountView()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 시도 횟수는 숫자여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc"})
+    void 시도_횟수_영문_입력(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        new Scanner(System.in);
+
+        assertThatThrownBy(() -> view.inputTryCountView()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 시도 횟수는 숫자여야 합니다.");
     }
 
     @Test
-    void 시도_횟수_입력_화면(){
-        view.inputTryCountView();
-        assertEquals("시도할 회수는 몇회인가요?", outputStreamCaptor.toString()
-                .trim());
-    }
-
-    @Test
-    void 실행_결과_화면(){
-        view.resultView();
-        assertEquals("실행 결과", outputStreamCaptor.toString()
-                .trim());
+    void 실행_결과_문장_표시_화면(){
+        view.outputResultView();
+        assertEquals("실행 결과", outputStreamCaptor.toString().trim());
     }
 }
