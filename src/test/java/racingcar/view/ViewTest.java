682f3684da6d73d4dd3872219804040ed400ff4c
seogineer <dgseo8981@gmail.com>
2022-10-10 12:31:47 +0900

    style: 불필요한 import 제거

--- src/test/java/racingcar/view/ViewTest.java
+++ src/test/java/racingcar/view/ViewTest.java
@@ -1,9 +1,7 @@
 package racingcar.view;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
